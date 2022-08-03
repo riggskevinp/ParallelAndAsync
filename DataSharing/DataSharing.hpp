e34671b5d9be189964e119cc398ff9c0456570f8
Kevin Riggs <riggskevinp@johndeere.com>
2022-08-03 13:55:50 -0500

    Finish Datasharing
    Fix empty stack exception handling

--- DataSharing/DataSharing.hpp
+++ DataSharing/DataSharing.hpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 #include <thread>
 #include <list>
 #include <mutex>
@@ -8,6 +9,10 @@
 #include <memory>
 #include <exception>
 #include <stack>
+#include <map>
+#include <string>
+#include <shared_mutex>
+#include <utility>
 
 namespace DataSharing{
 
@@ -19,7 +24,7 @@ namespace DataSharing{
 
     };
 
-}
+} // DataSharing
 
 namespace MutexExample{
     /*
@@ -60,7 +65,7 @@ namespace MutexExample{
 namespace AdaptedStack{
 
     struct empty_stack: std::exception {
-        [[nodiscard]] const char* what() const override {throw("empty");}
+        [[nodiscard]] const char* what() const override {return "Stack Empty";}
     };
 
     template<typename T>
@@ -103,3 +108,128 @@ namespace AdaptedStack{
 
     };
 }
+
+namespace DeadlockProblem{
+
+    class big_object{
+        big_object(){}
+    };
+
+
+    void swap(big_object& lhs, big_object& rhs);
+    class X{
+    private:
+        big_object detail;
+        std::mutex m;
+    public:
+        X(big_object const& sd):detail(sd){}
+        friend void swap(X& lhs, X& rhs){
+            if(&lhs==&rhs){
+                return;
+            }
+            std::scoped_lock guard(lhs.m,rhs.m);
+            swap(lhs.detail,rhs.detail);
+        }
+        friend void std_uniqueLock_lock_swap(X& lhs, X& rhs){
+            if(&lhs==&rhs){
+                return;
+            }
+            std::unique_lock<std::mutex>  lock_a(lhs.m,std::defer_lock);
+            std::unique_lock<std::mutex>  lock_b(rhs.m,std::defer_lock);
+            std::lock(lock_a,lock_b);
+            swap(lhs.detail,rhs.detail);
+        }
+    };
+
+    class Y{
+    private:
+        int detail;
+        mutable std::mutex m;
+        int get_detail() const {
+            std::lock_guard<std::mutex> lock_a(m);
+            return detail;
+        }
+    public:
+        Y(int d):detail(d){}
+        friend bool operator==(const Y& lhs, const Y& rhs){
+            if(&lhs==&rhs){
+                return true;
+            }
+            int const lhs_value = lhs.get_detail();
+            int const rhs_value = rhs.get_detail();
+            return lhs_value == rhs_value;
+        }
+    };
+}
+
+
+namespace SharedDataProtection{
+    class Resource{
+    public:
+        Resource() = default;
+        static void say_something(){std::cout << "Resource say something" << std::endl;}
+    };
+
+    std::shared_ptr<Resource> resource_ptr;
+    std::mutex resource_mutex;
+    // thread safe lazy initialization
+    void foo(){
+        std::unique_lock<std::mutex> lk(resource_mutex);
+        if(!resource_ptr){
+            resource_ptr = std::make_shared<Resource>();
+        }
+        lk.unlock();
+        resource_ptr->say_something();
+    }
+
+    // Thread-safe lazy initialization class member
+    /*
+    class connection_info;
+    class connection_handle;
+
+    class X{
+    private:
+        connection_info connection_details;
+        connection_handle connection;
+        std::once_flag connection_init_flag;
+        void open_connection(){
+            connection = connection_manager.open(connection_details);
+        }
+    public:
+        X(const connection_info& connection_details_):
+                connection_details(connection_details_){}
+        void send_data(const data_packet & data){
+            std::call_once(connection_init_flag, &X::open_connection,this);
+            connection.send_data();
+        }
+        data_packet receive_data(){
+            std::call_once(connection_init_flag, &X::open_connection,this);
+            return connection.receive_data();
+        }
+    };*/
+
+    // Data structure with shared mutex
+    class Dns_entry{
+    private:
+        std::string entry;
+    public:
+        Dns_entry():entry("empty"){}
+        explicit Dns_entry(std::string  e):entry(std::move(e)){}
+        Dns_entry operator()() const {return *this;}
+        std::string to_string() const {return entry;}
+    };
+    class Dns_cache{
+        std::map<std::string, Dns_entry> entries;
+        mutable std::shared_mutex entry_mutex;
+    public:
+        Dns_entry find_entry(const std::string& domain) const{
+            std::shared_lock<std::shared_mutex> lk(entry_mutex);
+            const std::map<std::string, Dns_entry>::const_iterator it = entries.find(domain); // or const auto
+            return (it == entries.end()) ? Dns_entry() : it->second();
+        }
+        void update_or_add_entry(const std::string& domain, const Dns_entry& dns_details){
+            std::lock_guard<std::shared_mutex> lk(entry_mutex);
+            entries[domain] = dns_details;
+        }
+    };
+}// SharedDataProtection
--- DataSharing/main.cpp
+++ DataSharing/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <thread>
+#include <random>
 
 #include "DataSharing.hpp"
-#include "../Hello/basics.hpp"
 
 void hello(){
     std::cout << "Hello most basic thread" << std::endl;
@@ -12,33 +12,74 @@ void hello_string(const std::string& s){
     std::cout << "Hello from " << s << std::endl;
 }
 
-void stack_pusher(AdaptedStack::threadsafe_stack<int> *st, int start, std::string name){
+void stack_pusher(AdaptedStack::threadsafe_stack<int> *st, int start, const std::string& name){
     for(int i = start; i < start + 300; i++){
         st->push(i);
+
         if(i % 10 == 0){
+            std::string n = name;
+            n = "d";
             std::cout << name << " " << i << std::endl;
         }
     }
 }
-void stack_reader(AdaptedStack::threadsafe_stack<int> *st, std::string name){
+void stack_reader(AdaptedStack::threadsafe_stack<int> *st, const std::string& name){
     for(int i = 1; i < 300; i++){
         if(i % 10 == 0){
+            std::string n = name;
+            n = "d";
             int j;
-            st->pop(j);
+            try{
+                st->pop(j);
+            } catch (std::exception& e){
+                std::cout << e.what() << std::endl;
+            }
             std::cout << name << " " << j << std::endl;
         }
     }
 }
 
+void cache_interaction(SharedDataProtection::Dns_cache* cache){
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution dist(1, 10);
+    for(int i = 0; i < 10; i++){
+        int x = dist(mt);
+        cache->update_or_add_entry(std::to_string(x), SharedDataProtection::Dns_entry(std::to_string(i)));
+    }
+}
+
+void cache_read(SharedDataProtection::Dns_cache* cache){
+    for(int i = 1; i <= 10; i++){
+        std::cout << "Cache entry value for " << std::to_string(i) << " : " << cache->find_entry(std::to_string(i)).to_string() << std::endl;
+    }
+}
+
 int main(){
 
-    auto add_int_thread = thread_ownership::joining_thread(MutexExample::add_some_ints);
-    auto check_list_thread = thread_ownership::joining_thread(MutexExample::check_for_some_ints);
+
+    auto ycomp_a = DeadlockProblem::Y(22);
+    auto ycomp_b = DeadlockProblem::Y(33);
+    auto ycomp_c = DeadlockProblem::Y(22);
+    std::cout << "a == a: " << (ycomp_a == ycomp_a) << std::endl;
+    std::cout << "a == b: " << (ycomp_a == ycomp_b) << std::endl;
+    std::cout << "a == c: " << (ycomp_a == ycomp_c) << std::endl;
+
+    auto add_int_thread = std::jthread(MutexExample::add_some_ints);
+    auto check_list_thread = std::jthread(MutexExample::check_for_some_ints);
 
     AdaptedStack::threadsafe_stack<int> shared_stack;
-    auto stack_thread = thread_ownership::joining_thread(stack_pusher, &shared_stack, 1, "first");
-    auto stack_thread2 = thread_ownership::joining_thread(stack_pusher, &shared_stack, 301, "second");
-    auto stack_thread3 = thread_ownership::joining_thread(stack_reader, &shared_stack, "third");
-    auto stack_thread4 = thread_ownership::joining_thread(stack_reader, &shared_stack, "fourth");
+    auto stack_thread = std::jthread(stack_pusher, &shared_stack, 1, "first");
+    auto stack_thread2 = std::jthread(stack_pusher, &shared_stack, 301, "second");
+    auto stack_thread3 = std::jthread(stack_reader, &shared_stack, "third");
+    auto stack_thread4 = std::jthread(stack_reader, &shared_stack, "fourth");
+
+    SharedDataProtection::foo();
+
+    auto cache = SharedDataProtection::Dns_cache();
+
+    auto cache_thread = std::jthread(cache_interaction, &cache);
+    auto cache_thread2 = std::jthread(cache_interaction, &cache);
+    auto cache_reader = std::jthread(cache_read, &cache);
 
 }
